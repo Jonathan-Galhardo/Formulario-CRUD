0b1040776e41293d5853fe0676f1ba89268200d0
Jonathan-Galhardo <galhardoj@gmail.com>
2022-08-29 14:43:51 -0300

    Estilização formulário

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <h1>Jonathan Galhardo</h1>
+    <form >
+      <input class='cpf' type='text' name='cpf' id='cpf' placeholder='CPF'></input>
+      <input class='name' type='text' name='name' id='name' placeholder='Nome'></input>
+      <input class="email" type="text" name="email" id="email" placeholder='E-mail'></input>
+      <input class="telefone" type="number" name="telefone" id="telefone" placeholder='Número do telefone'></input>
+
+      <button class='button' name='button' type='submit'>Salvar</button>
+    </form>
   );
 }
 
